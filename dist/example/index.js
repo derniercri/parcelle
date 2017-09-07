45f9f1cb4cc4cfd2256dc0fd81561f6895ac452b
Guillaume Bailleul <laibulle@gmail.com>
2017-09-07 11:31:21 +0200

    add missing dependency

--- dist/example/index.js
+++ dist/example/index.js
@@ -1,1 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const index_1 = require("./../index");
+const parcel = index_1.default('');
+parcel.findAddress('24 rue de Strasbourg Armentières');
 //# sourceMappingURL=index.js.map
---
+++ example/index.ts
@@ -0,0 +1,5 @@
+import Parcel from './../index'
+
+const parcel = Parcel('')
+
+parcel.findAddress('24 rue de Strasbourg Armentières')
